3e07cf943ef6681fb6d2754ee2e2185de1f9960d
Goo Zhen Jie <goozhenjie5@gmail.com>
2023-07-12 14:24:42 +0800

    _

--- web/index.php
+++ web/index.php
@@ -11,19 +11,19 @@ use Monolog\Logger;
 use Slim\Factory\AppFactory;
 use Slim\Views\Twig;
 
-require(__DIR__.'/../vendor/autoload.php');
+require(__DIR__ . '/../vendor/autoload.php');
 
 // Create DI container
 $container = new Container();
 // Add Twig to Container
-$container->set(Twig::class, function() {
-  return Twig::create(__DIR__.'/../views');
+$container->set(Twig::class, function () {
+    return Twig::create(__DIR__ . '/../views');
 });
 // Add Monolog to Container
 $container->set(LoggerInterface::class, function () {
-  $logger = new Logger('default');
-  $logger->pushHandler(new StreamHandler('php://stderr'), Level::Debug);
-  return $logger;
+    $logger = new Logger('default');
+    $logger->pushHandler(new StreamHandler('php://stderr'), Level::Debug);
+    return $logger;
 });
 
 // Create main Slim app
@@ -31,9 +31,20 @@ $app = Bridge::create($container);
 $app->addErrorMiddleware(true, false, false);
 
 // Our web handlers
-$app->get('/', function(Request $request, Response $response, LoggerInterface $logger, Twig $twig) {
-  $logger->debug('logging output.');
-  return $twig->render($response, 'index.twig');
+$app->get('/', function (Request $request, Response $response, LoggerInterface $logger, Twig $twig) {
+    $logger->debug('logging output.');
+    return $twig->render($response, 'index.twig');
+});
+
+// Add Cowsay to Container
+$container->set(\Cowsayphp\AnimalInterface::class, function () {
+    return \Cowsayphp\Farm::create(\Cowsayphp\Farm\Cow::class);
+});
+
+$app->get('/coolbeans', function (Request $request, Response $response, LoggerInterface $logger, \Cowsayphp\AnimalInterface $animal) {
+    $logger->debug('letting the Cowsay library write something cool.');
+    $response->getBody()->write("<pre>" . $animal->say("Cool beans") . "</pre>");
+    return $response;
 });
 
 $app->run();
